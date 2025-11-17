4031cf9431cd2c1647f0fcf1b37a630e1e4e48b4
Maher-Khemakhem <khemakhemmaher2003@gmail.com>
2025-11-17 19:24:45 +0100

    salzm

---
+++ hardhat.config.cjs
@@ -0,0 +1,21 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: process.env.SEPOLIA_PRIVATE_KEY ? [process.env.SEPOLIA_PRIVATE_KEY] : [],
+    },
+  },
+};
